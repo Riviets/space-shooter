ff1a697ebfe7076f6123f688a24675cae7db9c0c
Riviets <123061245+Riviets@users.noreply.github.com>
2026-07-22 08:26:43 +0300

    init

---
+++ main.py
@@ -0,0 +1,26 @@
+from kivymd.app import MDApp
+from kivy.core.window import Window
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.screenmanager import MDScreenManager
+
+Window.size = (300, 600)
+
+class MainScreen(MDScreen):
+    ...
+
+class GameScreen(MDScreen):
+    ...
+
+class ShooterApp(MDApp):
+    def build(self):
+
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Purple"
+
+        sm = MDScreenManager()
+        sm.add_widget(GameScreen(name='game'))
+        sm.add_widget(MainScreen(name='main'))
+        return sm
+
+app = ShooterApp()
+app.run()
