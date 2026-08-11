b3becf0a26b507e0428290ea03f67a6aeacba544
Riviets <123061245+Riviets@users.noreply.github.com>
2026-08-11 10:42:46 +0300

    Refactor ship classes and enemy spawning
    Refactor ship classes and improve enemy spawning logic.

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
-from kivymd.app import MDApp
-from kivymd.uix.widget import MDWidget
-from kivymd.uix.screenmanager import MDScreenManager
-from kivymd.uix.screen import MDScreen
-from kivy.clock import Clock
-from kivy.metrics import dp
-from kivy.core.window import Window
-from kivy.uix.image import Image
 from random import randint
+
+from kivy.clock import Clock
+from kivy.core.window import Keyboard, Window
+from kivy.metrics import dp
+from kivy.uix.image import Image
+from kivymd.app import MDApp
 from kivymd.uix.button import MDFlatButton
 from kivymd.uix.dialog import MDDialog
-from kivy.core.window import Keyboard
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.screenmanager import MDScreenManager
+from kivymd.uix.widget import MDWidget
 
 Window.size = 300, 600
 
-class MainScreen(MDScreen):
-    ...
 
-class GameOverScreen(MDScreen):
-    ...
+class MainScreen(MDScreen): ...
+
+
+class GameOverScreen(MDScreen): ...
+
 
 FPS = 60
 
@@ -31,16 +32,17 @@ SPAWN_ENEMY_TIME = 2
 
 
 class Shot(MDWidget):
-    def __init__(self, direction, owner, **kwargs):
+    def __init__(self, direction, **kwargs):
         super().__init__(**kwargs)
         self.direction = direction
-        self.owner = owner
 
 
 class Ship(Image):
-    def __init__(self, direction = DIR_UP, **kwargs):
+    def __init__(self, direction=DIR_UP, **kwargs):
         super().__init__(**kwargs)
         self.direction = direction
+        self.size_hint = (None, None)
+        self.size = (dp(50), dp(50))
 
     def moveLeft(self):
         self.pos[0] -= SHIP_SPEED
@@ -49,28 +51,29 @@ class Ship(Image):
         self.pos[0] += SHIP_SPEED
 
     def shot(self):
-        shot = Shot(self.direction, owner=self)
+        shot = Shot(self.direction)
         shot.center_x = self.center_x
         shot.y = self.top if self.direction == DIR_UP else self.y - shot.height
         self.parent.parent.parent.parent.cartridge.append(shot)
         self.parent.add_widget(shot)
 
-    def update(self):          
+    def update(self):
         pass
 
 
 class PlayerShip(Ship):
     def __init__(self, **kwargs):
         super().__init__(direction=DIR_UP, **kwargs)
+        self.source = "spaceship.png"
 
     def update(self, keys):
         for key in keys:
             if keys[key] == True:
-                if key == 'left' and self.x > 0:
+                if key == "left" and self.center_x > 0:
                     self.moveLeft()
-                if key == 'right' and self.right < Window.width:
+                if key == "right" and self.center_x < Window.width:
                     self.moveRight()
-                if key == 'shot':
+                if key == "shot":
                     self.shot()
                     keys[key] = False
 
@@ -78,12 +81,13 @@ class PlayerShip(Ship):
 class EnemyShip(Ship):
     def __init__(self, *args, **kwargs):
         super().__init__(direction=DIR_DOWN, **kwargs)
+        self.source = "ufo.png"
         self.frame = 0
 
     def update(self):
         super().update()
         self.pos[1] -= dp(3)
-        if self.frame % 100 == 0:   
+        if self.frame % 100 == 0:
             self.shot()
         self.frame += 1
 
@@ -93,13 +97,12 @@ class GameScreen(MDScreen):
         super().__init__(*args, **kwargs)
 
         self.eventkeys = {}
-        self.cartridge = []
-
-        self.ship = None
+        self.ship = self.ids.ship
         self.enemyShips = []
 
-        self.pauseMenu = None
+        self.cartridge = []
 
+        self.pauseMenu = None
         self.spawn_delay = SPAWN_ENEMY_TIME
         self.time_last_spawn = 0
 
@@ -113,9 +116,11 @@ class GameScreen(MDScreen):
 
         return super().on_enter(*args)
 
-    def spawn_enemy(self):
+    def spawn_enemy(self, *args):
         ship = EnemyShip()
-        ship.pos = (randint(0, int(Window.size[0] - ship.size[0])), Window.size[1])
+        max_x = max(0, int(Window.width - ship.width))
+        ship.x = randint(0, max_x)
+        ship.y = Window.height
         self.enemyShips.append(ship)
         self.ids.front.add_widget(ship)
 
@@ -132,39 +137,13 @@ class GameScreen(MDScreen):
             if ship.top < 0:
                 self.enemyShips.remove(ship)
                 self.ids.front.remove_widget(ship)
+                continue
 
             if ship.collide_widget(self.ship):
                 self.game_over()
+                return
 
         self.manage_cartridge()
-
-    def manage_cartridge(self):
-        for bullet in self.cartridge[:]:
-            bullet.y += BULLET_SPEED * bullet.direction
-
-            self.check_collisions(bullet)
-
-            if bullet.y > Window.height or bullet.top < 0:
-                self.remove_shot(bullet)
-
-    def check_collisions(self, bullet):
-        if bullet.owner == self.ship:
-            for enemy in self.enemyShips[:]:
-                if bullet.collide_widget(enemy):
-                    self.enemyShips.remove(enemy)
-                    self.ids.front.remove_widget(enemy)
-
-                    self.remove_shot(bullet)
-                    break
-        else:
-            if bullet.collide_widget(self.ship):
-                self.game_over()
-                self.remove_shot(bullet)
-
-    def remove_shot(self, bullet):
-        if bullet in self.cartridge:
-            self.cartridge.remove(bullet)
-            self.ids.front.remove_widget(bullet)
 
     def game_over(self):
         self.updateEvent.cancel()
@@ -177,7 +156,37 @@ class GameScreen(MDScreen):
             self.ids.front.remove_widget(bullet)
             self.cartridge.remove(bullet)
 
-        self.manager.current = 'game_over'
+        self.manager.current = "game_over"
+
+    def manage_cartridge(self):
+        for bullet in self.cartridge[:]:
+            bullet.y += BULLET_SPEED * bullet.direction
+
+            if bullet.y > Window.height or bullet.top < 0:
+                self.ids.front.remove_widget(bullet)
+                self.cartridge.remove(bullet)
+                continue
+
+            if bullet.direction == DIR_UP:
+                hit_enemy = None
+                for ship in self.enemyShips:
+                    if bullet.collide_widget(ship):
+                        hit_enemy = ship
+                        break
+
+                if hit_enemy:
+                    self.enemyShips.remove(hit_enemy)
+                    self.ids.front.remove_widget(hit_enemy)
+                    self.ids.front.remove_widget(bullet)
+                    self.cartridge.remove(bullet)
+                    continue
+
+            if bullet.direction == DIR_DOWN:
+                if bullet.collide_widget(self.ship):
+                    self.ids.front.remove_widget(bullet)
+                    self.cartridge.remove(bullet)
+                    self.game_over()
+                    return
 
     def pressKey(self, key):
         self.eventkeys[key] = True
@@ -187,7 +196,7 @@ class GameScreen(MDScreen):
 
     def show_menu(self):
         self.updateEvent.cancel()
-        
+
         if not self.pauseMenu:
             self.pauseMenu = MDDialog(
                 title="Game Paused",
@@ -198,8 +207,8 @@ class GameScreen(MDScreen):
                         text="RESUME",
                         theme_text_color="Custom",
                         text_color=app.theme_cls.primary_color,
-                        on_press=self.pauseStop
-                    )
+                        on_press=self.pauseStop,
+                    ),
                 ],
             )
         self.pauseMenu.open()
@@ -211,12 +220,20 @@ class GameScreen(MDScreen):
         self.updateEvent = Clock.schedule_interval(self.update, 1 / FPS)
 
     def _on_key_down(self, window, keycode, *args, **kwargs):
-        key = key if (key := Keyboard.keycode_to_string(window, keycode)) != 'spacebar' else 'shot'
-        
+        key = (
+            key
+            if (key := Keyboard.keycode_to_string(window, keycode)) != "spacebar"
+            else "shot"
+        )
+
         self.eventkeys[key] = True
 
     def _on_key_up(self, window, keycode, *args, **kwargs):
-        key = key if (key := Keyboard.keycode_to_string(window, keycode)) != 'spacebar' else 'shot'
+        key = (
+            key
+            if (key := Keyboard.keycode_to_string(window, keycode)) != "spacebar"
+            else "shot"
+        )
 
         self.eventkeys[key] = False
 
@@ -228,12 +245,12 @@ class ShooterApp(MDApp):
 
         self.sm = MDScreenManager()
 
-        self.sm.add_widget(GameScreen(name='game'))
-        self.sm.add_widget(MainScreen(name='main'))
-        self.sm.add_widget(GameOverScreen(name='game_over'))
+        self.sm.add_widget(MainScreen(name="main"))
+        self.sm.add_widget(GameScreen(name="game"))
+        self.sm.add_widget(GameOverScreen(name="game_over"))
 
         return self.sm
-    
+
 
 app = ShooterApp()
 app.run()
